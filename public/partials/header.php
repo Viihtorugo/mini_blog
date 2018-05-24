2bb762b775d7437be22bc0b46ce99df22b87f078
Viihtorugo <Victor-Torugo@hotmail.com>
2018-05-24 20:16:26 -0300

    feature(css): add folder css

--- public/partials/header.php
+++ public/partials/header.php
@@ -2,9 +2,12 @@
 <html>
 <head>
     <meta charset="utf-8" />
+    <link rel="stylesheet" href="css/main.css">
     <title>Global Blog</title>
 </head>
-<body>
-<header>
-    
+<body class="body">
+<header class="menu">
+    <form class="menu-item" action="">
+        <input type="text" name="search">
+    </form>
 </header>
