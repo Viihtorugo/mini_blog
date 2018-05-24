765f96d8b514ef86037792ecc7b98c40790ffb4d
Viihtorugo <Victor-Torugo@hotmail.com>
2018-05-24 19:54:25 -0300

    feature(header): Adding tag header and struct the home

--- public/index.php
+++ public/index.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Global Blog</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
-</head>
-<body>
+<?php include_once('partials/header.php'); ?>
     <h1>Global Blog</h1>
-</body>
-</html>
+<?php include_once('partials/footer.php'); ?>
