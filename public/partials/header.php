449dcaef4798ce1899702f119d36259c7c3df02d
AshcroftGamer <lippeativista@gmail.comcd>
2018-05-24 20:40:53 -0300

    create button and checkbox

--- public/partials/header.php
+++ public/partials/header.php
@@ -8,6 +8,12 @@
 <body class="body">
 <header class="menu">
     <form class="menu-item" action="">
-        <input type="text" name="search">
+        <input align="center"  type="text" name="search">
+        <select align="center">
+            <option value="Educação">Educação</option>
+            <option value="Finanças">Finanças</option>
+            <option value="Merda">Merda</option>
+        </select>
+        <input type="submit" value="pesquisar" />
     </form>
 </header>
